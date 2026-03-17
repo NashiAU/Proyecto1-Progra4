aefe220c3ef62da30330b417da7f67f86a81eba0
Sebas Garro U <sebasgarro2006@gmail.com>
2026-03-17 10:13:05 -0600

    Initial Commit

---
+++ src/main/java/com/proyecto1/proyecto1progra4/Proyecto1Progra4Application.java
@@ -0,0 +1,13 @@
+package com.proyecto1.proyecto1progra4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1Progra4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto1Progra4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto1/proyecto1progra4/Proyecto1Progra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto1.proyecto1progra4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1Progra4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
