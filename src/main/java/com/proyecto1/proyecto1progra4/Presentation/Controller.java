ba3b01ecb54fd13085c9e55969c753903697ee73
Sebastian Garro <sebasgarro2006@gmail.com>
2026-03-25 16:46:48 -0600

    Add RootRedirectController for root redirection

--- src/main/java/com/proyecto1/proyecto1progra4/Presentation/Controller.java
+++ src/main/java/com/proyecto1/proyecto1progra4/Presentation/Controller.java
@@ -1,4 +1,12 @@
 package com.proyecto1.proyecto1progra4.Presentation;
 
-public class Controller {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller("RootRedirectController")
+public class RootRedirectController {
+    @GetMapping("/")
+    public String redirect(Model model) {
+        return "redirect:/presentation/prestamos/list";
+        }
 }
