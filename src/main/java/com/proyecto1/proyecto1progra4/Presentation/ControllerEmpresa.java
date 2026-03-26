5fe61f44047635c472dedf2a98c547769ad67a4d
Nashi <sebasgarro2006@gmail.com>
2026-03-25 18:23:36 -0600

    Reestructuracion minima de templates

---
+++ src/main/java/com/proyecto1/proyecto1progra4/Presentation/ControllerEmpresa.java
@@ -0,0 +1,7 @@
+package com.proyecto1.proyecto1progra4.presentation;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.security.Principal;
+
+public class ControllerEmpresa {
+}
